b2ffa3a95cd3bb3a78edf644ca86e4aff11d642f
Samuele Medici <samuele.medici@quinck.io>
2026-04-12 22:13:24 +0200

    ci: fix on DB Port

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :recco, Recco.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  port: 5460,
+  port: String.to_integer(System.get_env("DB_PORT") || "5460"),
   database: "recco_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
